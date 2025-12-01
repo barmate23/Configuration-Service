90fe872564f3ca3dcdcee5f04afa4bf12f4093f3
barmate23 <akshay.barmate@sarvosmi.io>
2025-12-01 17:11:19 +0530

    packing slip upload

--- src/main/java/com/stockmanagementsystem/repository/AcceptedRejectedContainerBarcodeRepository.java
+++ src/main/java/com/stockmanagementsystem/repository/AcceptedRejectedContainerBarcodeRepository.java
@@ -3,6 +3,8 @@ package com.stockmanagementsystem.repository;
 
 import com.stockmanagementsystem.entity.AcceptedRejectedContainerBarcode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +17,9 @@ public interface AcceptedRejectedContainerBarcodeRepository extends JpaRepositor
     List<AcceptedRejectedContainerBarcode> findByIsDeletedAndIsAcceptedAndSubOrganizationIdAndAcceptedRejectedContainerPurchaseOrderLineId(boolean b, Boolean isAccepted, Integer subOrgId, Integer poLineId);
 
     boolean existsByPackingSlipNumber(String nextSlip);
+
+    @Query("SELECT MAX(CAST(SUBSTRING(a.packingSlipNumber, LENGTH(:prefix) + 1) AS int)) " +
+            "FROM AcceptedRejectedContainerBarcode a " +
+            "WHERE a.packingSlipNumber LIKE CONCAT(:prefix, '%')")
+    Integer findMaxSequenceForPrefix(@Param("prefix") String prefix);
 }
