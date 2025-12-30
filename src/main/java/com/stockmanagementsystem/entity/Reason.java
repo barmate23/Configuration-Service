3764191bc8b07f8f0bb3673b24b9c94b6ac51a04
ContextAI India <manmohan.shrivas@sarvosmi.io>
2025-12-30 13:23:00 +0530

    add json ignore

--- src/main/java/com/stockmanagementsystem/entity/Reason.java
+++ src/main/java/com/stockmanagementsystem/entity/Reason.java
@@ -1,5 +1,6 @@
 package com.stockmanagementsystem.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -13,13 +14,13 @@ public class Reason {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
+    @JsonIgnore
     @Column(name = "OrganizationId")
     private Integer organizationId;
-
+    @JsonIgnore
     @Column(name = "SubOrganizationId")
     private Integer subOrganizationId;
-
+    @JsonIgnore
     @Column(name = "Sequence")
     private Integer sequence;
 
@@ -30,24 +31,25 @@ public class Reason {
     private String rejectedReason;
 
     @ManyToOne
+    @JsonIgnore
     @JoinColumn(name = "reasonCategoryMasterId")
     private ReasonCategoryMaster reasonCategoryMaster;
-
+    @JsonIgnore
     @Column(name = "isUserCreated")
     private Boolean isUserCreated;
-
+    @JsonIgnore
     @Column(name = "IsDeleted")
     private Boolean isDeleted;
-
+    @JsonIgnore
     @Column(name = "CreatedBy")
     private Integer createdBy;
-
+    @JsonIgnore
     @Column(name = "CreatedOn")
     private Date createdOn;
-
+    @JsonIgnore
     @Column(name = "ModifiedBy")
     private Integer modifiedBy;
-
+    @JsonIgnore
     @Column(name = "ModifiedOn")
     private Date modifiedOn;
 
